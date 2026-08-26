fc63c8b1fce7574a116ba7430641bf609b50d636
jinsublee <jinsublee@gmail.com>
2026-08-26 17:29:46 +0900

    8/26 콘텐츠 업데이트 코드 만듬

--- Work/Playwright/Content_crawler.py
+++ Work/Playwright/Content_crawler.py
@@ -27,6 +27,7 @@ if not SUPABASE_URL or not SUPABASE_KEY:
     
 supabase:Client= create_client(SUPABASE_URL, SUPABASE_KEY)
 
+
 def get_postnum() -> list:
     """크롤링 해야하는 대상 게시글 번호 찾기"""
     try:
@@ -40,37 +41,32 @@ def get_postnum() -> list:
     except Exception as e:
         print('게시글 번호 조회 실패')
 
-def Content_cralwer(post_num) ->str:
+# 맨 위에 글 번호 전체를 가져오는 로직을 한 다음에, 다음부턴 증분만 가져와서 컬럼을 등록하는 방법을 쓰자
+
+
+async def content_crawler(page, url:str, post_num: int) ->str:
     """게시물 번호를 기반으로 콘텐츠를 크롤링합니다."""
     
-    content_locator=page.locator("div.write_div")
     try:
-        await content_locator.first.wait_for()
-        print(f"게시글 번호 {post_num} 크롤링을 시작합니다.")
-        print('='*40)
-        print('게시글 미리 보기')
-        print(content_locator.inner_text())
+        response=await page.goto(url,
+                                 wait_untill="domcontentloaded",
+                                 timeout=60_000)
         
     except:
-        print('게시글이 제대로 수집되지 않았습니다.')
-
-if __name__=="__main__":
-    Content_crawler(17655144)
-    
+        print("url에 접속할 수 없습니다.")
         
-            
+    content_locator=page.locator("div.write_div").first
+    await content_locator.wait_for(timeout=10_000)
+    content=(await content_locator.inner_text()).strip()
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-def main()->None:
-    url=f'https://gall.dcinside.com/board/view/?id={GALLERY_ID}_new&no={post_num}'
+    try:
+        
+        print(f"게시글 번호 {post_num} 크롤링을 시작합니다.")
+        print('='*40)
+        print('게시글 수집을 성공했습니다.')
+        return content
+        
+    except Exception as e:
+        print(f'게시글이 제대로 수집되지 않았습니다 post_num={post_num} | {e}')
+        return None
     
